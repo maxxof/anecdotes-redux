4790fff9a5195ccc70f98cbe1076dad147d18ef0
maxxof <maxim.egorov.soccer@gmail.com>
2022-12-19 15:18:16 +0200

    Ex 6.9 done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { legacy_createStore as createStore } from 'redux'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
